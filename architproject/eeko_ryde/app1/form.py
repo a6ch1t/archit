5db5b5473669eb4eaf21806131159d80236c59a3
architkrishna <139440091+a6ch1t@users.noreply.github.com>
2024-01-16 13:40:58 +0530

    Delete architproject/eeko_ryde directory

--- architproject/eeko_ryde/app1/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class ryde(forms.ModelForm):
-    class Meta:
-        model=eeko
-        fields='__all__'
--- architproject/eeko_ryde/app1/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from .models import *
-from app1.form import *
-def home(request):
-    return render(request,'index.html')
